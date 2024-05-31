c239006cc4004c8ba066b413bc60b12287ab479c
Sang-Min Kang <rdrniel12345@gmail.com>
2024-05-31 14:37:44 +0900

    feat: add socket connection code(running server)

--- src/main/java/dvm/ControllerMapper.java
+++ src/main/java/dvm/ControllerMapper.java
@@ -4,7 +4,6 @@ import controller.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class ControllerMapper {
 
--- src/main/java/dvm/DVMSimulationServer.java
+++ src/main/java/dvm/DVMSimulationServer.java
@@ -1,11 +1,51 @@
 package dvm;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class DVMSimulationServer {
 
     private ControllerMapper controllerMapper;
 
     public static void main(String[] args) {
-        System.out.println("Server is now loading...");
+        ControllerMapper mapper = new ControllerMapper();
+
+        int port = 9000;
+        ExecutorService service = Executors.newFixedThreadPool(50);
+
+        if (args.length != 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        try (ServerSocket welcomeSocket = new ServerSocket(port)){
+            Socket connection;
+            while ((connection = welcomeSocket.accept()) != null) {
+                Socket finalConnection = connection;
+                // set task to new thread & execute controller
+                service.submit(() -> {
+                    try(InputStream is = finalConnection.getInputStream();
+                        OutputStream os = finalConnection.getOutputStream()) {
+                        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                        DataOutputStream dos = new DataOutputStream(os);
+
+                        String startLine = br.readLine();
+                        String[] startLines = startLine.split(" ");
+                        String method = startLines[0];
+                        String url = startLines[1];
+
+                        dos.writeChars("Hello! -> " + method + " : " + url);
+                        dos.flush();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
