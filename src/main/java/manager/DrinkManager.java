43b88c65cc8bdb09340b8dd66a712356eef07b4e
nuuuuo <67111221+nuuuuo@users.noreply.github.com>
2024-06-04 20:45:54 +0900

    Add files via upload

--- src/main/java/manager/DrinkManager.java
+++ src/main/java/manager/DrinkManager.java
@@ -1,6 +1,10 @@
 package manager;
 
+import controller.DrinkController;
 import data.Drink;
+import db.DBManager;
+import db.DrinkDBManager;
+
 import java.util.*;
 
 public class DrinkManager {
@@ -8,33 +12,50 @@ public class DrinkManager {
     private List<Drink> menuList;
 
     public DrinkManager() {
+        this.menuList = reqAmountOfDrink();
     }
 
     public Boolean hasDrink(String drinkType, int drinkNum) {
         // TODO implement here
-        return null;
+        DrinkDBManager drinkDBManager = DrinkDBManager.getManager();
+        return drinkDBManager.hasDrink(drinkType, drinkNum);
     }
 
     public Drink getDrink(String drinkType, int drinkNum) {
         // TODO implement here
-        return null;
+        DrinkDBManager drinkDBManager = DrinkDBManager.getManager();
+        return drinkDBManager.getDrink(drinkType, drinkNum);
     }
 
-    public void getMenuInfo() {
+    public List<Drink> getMenuInfo() {
         // TODO implement here
-    }
+        return menuList;
+    }///////////getMenuInfo의 리턴값을 void -> List<Drink>로 변경
 
-    public void getDrinkQuantity(String drinkType) {
+    public int getDrinkQuantity(String drinkType) {
         // TODO implement here
-    }
+        DrinkDBManager drinkDBManager = DrinkDBManager.getManager();
+        return drinkDBManager.getDrinkQuantity(drinkType);
+    }///////////getDrinkQuantity의 리턴값을 void -> int로 변경
 
-    public void manageDrink(String drinkType, String drinkNum) {
+    public boolean manageDrink(String drinkType, int drinkNum) {
         // TODO implement here
-    }
+        DrinkDBManager drinkDBManager = DrinkDBManager.getManager();
+        return drinkDBManager.setDrink(drinkType, drinkNum);
+    }///////////manageDrink 파라미터 drinkNum의 타입을 String -> int로 변경
+    ///////////manageDrink 리턴값을 void -> boolean 변경
 
     public List<Drink> reqAmountOfDrink() {
         // TODO implement here
-        return null;
+        DrinkDBManager drinkDBManager = DrinkDBManager.getManager();
+        this.menuList = drinkDBManager.getAllDrinkStatus();
+        return menuList;
     }
 
 }
+
+
+
+//////////지금까지 5개 고침. DrinkManager getMenuInfo, DrinkManager getDrinkQuantity, AdminDBManager checkUser, DrinkManager manageDrink, DrinkManager manageDrink
+
+///////AdminAccountManager logout 질문
--- src/main/java/manager/VerificationManager.java
+++ src/main/java/manager/VerificationManager.java
@@ -1,22 +1,58 @@
 package manager;
 
 import data.VerificationCode;
+import db.VerificationCodeDBManager;
+
+import java.util.Random;
 
 public class VerificationManager {
 
     public String getVerificationCode() {
         // TODO implement here
-        return "";
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while(true){
+            Random random = new Random();
+
+            for(int i=0; i<10; i++){ //랜덤한 난수 생성
+                if(random.nextBoolean()){
+                    stringBuilder.append(random.nextInt(10));
+                }else{ //랜덤한 알파벳 생성
+                    if(random.nextBoolean()){ //랜덤한 대문자 알파벳 생성
+                        stringBuilder.append((char)(random.nextInt(26)+65));
+                    }else{ //랜덤한 소문자 알파벳 생성
+                        stringBuilder.append((char)(random.nextInt(26)+97));
+                    }
+                }
+            }
+
+            String tempCode = stringBuilder.toString();
+            VerificationCode verifyCodeResult = verifyCode(tempCode);
+
+            if(verifyCodeResult.getCode().isEmpty() && verifyCodeResult.getDrinkType().isEmpty() && verifyCodeResult.getDrinkNum()==0){ //중복 인증코드 없음
+                boolean codeSaveSuccessResult = saveCode(tempCode); //인증코드 저장
+
+                if(codeSaveSuccessResult){ //코드 저장 성공 여부 확인
+                    break; //코드 저장 성공
+                }
+                //코드 저장 실패. 코드 재생성 필요함
+            }
+            //중복 인증코드 있음. 코드 재생성 필요함
+            stringBuilder.setLength(0); //stringBuilder 초기화
+        }
+
+        return stringBuilder.toString();
     }
 
     public boolean saveCode(String code) {
         // TODO implement here
-        return false;
+        VerificationCodeDBManager verificationCodeDBManager = VerificationCodeDBManager.getManager();
+        return verificationCodeDBManager.saveCode(code); //verificationCodeDBManager에게 인증코드 저장 요청
     }
 
     public VerificationCode verifyCode(String code) {
         // TODO implement here
-        return null;
+        VerificationCodeDBManager verificationCodeDBManager = VerificationCodeDBManager.getManager();
+        return verificationCodeDBManager.checkCode(code); //verificationCodeDBManager에게 중복 인증코드인지 확인
     }
-
 }
